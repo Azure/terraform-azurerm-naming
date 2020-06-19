d7871b8e5d2db20a5bce3480dd43eed9a4d7d21d
Luke Devonshire <6423205+LukeDevs@users.noreply.github.com>
2020-06-19 13:38:27 +0100

    Added NSG Rule (#8)

--- outputs.tf
+++ outputs.tf
@@ -650,6 +650,10 @@ output "network_security_group" {
   value = local.az.network_security_group
 }
 
+output "network_security_group_rule" {
+  value = local.az.network_security_group_rule
+}
+
 output "private_endpoint" {
   value = local.az.private_endpoint
 }
