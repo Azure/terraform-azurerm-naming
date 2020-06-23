b0f7fbde8ce0c452c4a6911ff9f12ecc43088b25
Mason Cusack <1masoncusack@gmail.com>
2020-06-23 13:23:29 +0100

    Add entry for databricks_cluster (#14)
    * add entry for databricks_cluster
    
    * add output and document
    
    * change resourceDefinition and document process in README

--- main.tf
+++ main.tf
@@ -611,6 +611,16 @@ locals {
       scope       = "resourceGroup"
       regex       = "/^(?=.{3,30}$)[a-zA-Z0-9-_]+$/"
     }
+    databricks_cluster = {
+      name        = substr(join("-", compact([local.prefix, "dbc", local.suffix])), 0, 30)
+      name_unique = substr(join("-", compact([local.prefix, "dbc", local.suffix_unique])), 0, 30)
+      dashes      = true
+      slug        = "dbc"
+      min_length  = 3
+      max_length  = 30
+      scope       = "parent"
+      regex       = "/^(?=.{3,30}$)[a-zA-Z0-9-_]+$/"
+    }
     kusto_cluster = {
       name        = substr(join("", compact([local.prefix_safe, "kc", local.suffix_safe])), 0, 22)
       name_unique = substr(join("", compact([local.prefix_safe, "kc", local.suffix_unique_safe])), 0, 22)
