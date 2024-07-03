4af2fbd51c5a23d2528b1fbdb3c410be59b067d7
Daan Toes <112694691+cndaan@users.noreply.github.com>
2024-07-03 13:53:32 +0200

    feat: update_provider_random_version (#14)
    * Updating source in module example to use the right module
    
    * chore: update random provider version to 3.6

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.3.2"
+      version = "~> 3.6"
     }
   }
   required_version = "~> 1.0"
